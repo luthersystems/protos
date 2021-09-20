148e70e8bb2923fca009fcee6bbcbdb58b86503c
Jeremy Asher <jeremy.asher@luthersystems.com>
2021-09-20 14:18:14 -0700

    add healthcheck protos
    This adds standard healthcheck messages.

--- build.mk
+++ build.mk
@@ -1,6 +1,6 @@
 # Copyright © 2021 Luther Systems, Ltd. All right reserved.
 
-PROTOC=protoc -I${GOPATH}/src -I/opt/proto/googleapis -I/opt/proto/grpc-gateway
+PROTOC=protoc -I${PWD} -I/opt/proto/googleapis -I/opt/proto/grpc-gateway
 
 MODEL_PROTOS=$(wildcard spb/*.proto)
 
